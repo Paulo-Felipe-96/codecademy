26e584c57be5009b6910d5228108576bccfd5e4d
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-05-04 12:19:11 -0300

    feat: refactoring, revealing module pattern added

--- data_types.js
+++ data_types.js
@@ -14,55 +14,77 @@ console.log(-(2 ^ (52 - 1)));
 
 // Map and Filter examples with Red Dead Remption 2 characters
 
-const characters = [
-  {
-    name: "Sean",
-    age: 20,
-    isAlive: false,
-  },
-  {
-    name: "Charles",
-    age: 39,
-    isAlive: true,
-  },
-  {
-    name: "Arthur",
-    age: 34,
-    isAlive: false,
-  },
-];
-
 // with map example
+const Characters = (() => {
+  const characters = [
+    {
+      name: "Sean",
+      age: 20,
+      isAlive: false,
+    },
+    {
+      name: "Charles",
+      age: 39,
+      isAlive: true,
+    },
+    {
+      name: "Arthur",
+      age: 34,
+      isAlive: false,
+    },
+  ];
 
-const anyMapedCharacter = characters.map((anyChar) => anyChar);
-console.log("Any:");
-console.log(nameOfMapedCharacter(anyMapedCharacter));
-console.log();
+  // with filter example
 
-// with filter example
+  function isAliveCharacter(character, status) {
+    return status === "yes"
+      ? character.isAlive === true
+      : character.isAlive === false;
+  }
 
-function isAliveCharacter(character, status) {
-  return status === "yes" ? character.isAlive === true : character.isAlive === false;
-}
+  // only name returned of maped
+  function nameOfMapedCharacter(character) {
+    return character.map((char) => char.name);
+  }
 
-// only name returned of maped
-function nameOfMapedCharacter(character) {
-  return character.map((char) => char.name);
-}
+  // name and alive status returned
+  function characterAliveStatusAndName(character) {
+    return character.map((char) => {
+      const info = {
+        nome: char.name,
+        isAlive: char.isAlive === true ? "alive" : "dead",
+      };
+      return info;
+    });
+  }
 
-// name and alive status returned
-function characterAliveStatusAndName(character) {
-  return character.map((char) => {
-    const info = {
-      nome: char.name,
-      isAlive: char.isAlive === true ? "alive" : "dead",
-    };
-    return info;
-  });
-}
+  // named function callback and "yes" or "no" about alive status
+  const aliveCharacters = characters.filter((char) =>
+    isAliveCharacter(char, "no")
+  );
 
-// named function callback and "yes" or "no" about alive status
-const aliveCharacters = characters.filter((char) => isAliveCharacter(char, "no"));
+  const notAliveCharacters = characters.filter(
+    (character) => character.isAlive === false
+  );
+
+  return {
+    isAliveCharacter,
+    nameOfMapedCharacter,
+    characterAliveStatusAndName,
+    aliveCharacters,
+    notAliveCharacters,
+    characters,
+  };
+})();
+
+const {
+  isAliveCharacter,
+  nameOfMapedCharacter,
+  characterAliveStatusAndName,
+  aliveCharacters,
+  notAliveCharacters,
+  characters,
+} = Characters;
 
 console.log("Filtered chars:");
 console.log(nameOfMapedCharacter(aliveCharacters));
@@ -71,10 +93,12 @@ console.log(characterAliveStatusAndName(aliveCharacters));
 console.log();
 
 // with no named function callback
-const notAliveCharacters = characters.filter(
-  (character) => character.isAlive === false
-);
 
 console.log("Not alive only:");
 console.log(nameOfMapedCharacter(notAliveCharacters));
+console.log();
+
+const anyMapedCharacter = characters.map((anyChar) => anyChar);
+console.log("Any:");
+console.log(nameOfMapedCharacter(anyMapedCharacter));
 console.log();
