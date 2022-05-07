b81b54ab8ffd5459b267070278c02581ac25b23a
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-05-07 20:59:43 -0300

    feat: variables var introducing

---
+++ variables.js
@@ -0,0 +1,8 @@
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+
+console.log(favoriteFood);
+
+console.log(numOfSlices);
+
+console.log(`My favorite food is ${favoriteFood} and it has ${numOfSlices} slices.`);
