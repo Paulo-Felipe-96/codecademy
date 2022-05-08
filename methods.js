d5e2f40588a9f68e7bf7ebee8038d9ecd631bdac
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-05-07 21:03:19 -0300

    feat: methods introducing

---
+++ methods.js
@@ -0,0 +1,7 @@
+// Methods can be accessed from a instance of data types
+
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
